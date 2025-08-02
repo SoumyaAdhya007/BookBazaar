aeca687385cfaec587c13137249b086ad5866878
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-02 22:26:43 +0530

    feat(models): add book schema

---
+++ src/models/book.models.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  etag: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+  },
+  authors: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  publisher: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  industryIdentifiers: [
+    {
+      type: { type: String },
+      identifier: { type: String },
+    },
+  ],
+  readingModes: {
+    text: {
+      type: Boolean,
+    },
+    image: { type: Boolean },
+  },
+  pageCount: { type: Boolean },
+  printType: { type: String },
+  categories: [{ type: String }],
+  averageRating: { type: Number },
+  ratingsCount: { type: Number },
+  maturityRating: { type: String },
+  allowAnonLogging: { type: Boolean, default: false },
+  contentVersion: { type: String },
+  imageLinks: {
+    smallThumbnail: { type: String },
+    thumbnail: { type: String },
+  },
+  language: { type: String },
+  previewLink: { type: String },
+  infoLink: { type: String },
+  canonicalVolumeLink: { type: String },
+});
+
+bookSchema.index({ etag: 1, title: 1 });
+
+const Book = mongoose.model("book", bookSchema);
+
+export default Book;
