f421c80a84c6a256aea3223ece69489a20292e9e
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-04 20:59:12 +0530

    feat(app): add route handlers

--- src/app/app.js
+++ src/app/app.js
@@ -20,5 +20,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// routes
+import AuthRouter from "../routes/auth.routes.js";
+import BookRouter from "../routes/book.routes.js";
+import ReviewRouter from "../routes/review.routes.js";
+import CartRouter from "../routes/cart.routes.js";
+import PaymentRouter from "../routes/payment.routes.js";
+import OrderRouter from "../routes/order.routes.js";
+
+app.use("/api/v1", AuthRouter);
+app.use("/api/v1", BookRouter);
+app.use("/api/v1", ReviewRouter);
+app.use("/api/v1", CartRouter);
+app.use("/api/v1", PaymentRouter);
+app.use("/api/v1", OrderRouter);
+
 app.use(errorHandler);
 export default app;
