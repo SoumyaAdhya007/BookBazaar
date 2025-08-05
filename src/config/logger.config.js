b099dbb4ec3e80755848c9a1c1cb78c26035da87
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-05 20:49:29 +0530

    chore(config): change logger transport to console

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -23,11 +23,14 @@ const logger = winston.createLogger({
   format: fileFormat,
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.File({
-      filename: "logs/error.log",
-      level: "error",
+    new winston.transports.Console({
+      format: fileFormat,
     }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
+    // new winston.transports.File({
+    //   filename: "logs/error.log",
+    //   level: "error",
+    // }),
+    // new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
 
