1ff88341166d909ca5e643e13fae440415f606b7
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-08 23:44:41 +0530

    feat(routes): implement complete routes for orders

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,32 @@
 import { Router } from "express";
+import authAccessMiddleware from "../middlewares/auth.middlewares.js";
+import apiKeyCheckMiddleware from "../middlewares/apiKey.middleware.js";
+import { getAllOrders, getOrder, placeOrder } from "../controllers/order.controllers.js";
+import validateData from "../middlewares/validation.middlewares.js";
+import {
+  getOrderValidation,
+  placeOrderValidation,
+} from "../validations/order.validations.js";
 
 const router = Router();
 
+router
+  .route("/:cartId")
+  .post(
+    authAccessMiddleware,
+    apiKeyCheckMiddleware,
+    validateData(placeOrderValidation),
+    placeOrder
+  );
+router
+  .route("/")
+  .get(authAccessMiddleware, apiKeyCheckMiddleware, getAllOrders);
+router
+  .route("/:id")
+  .get(
+    authAccessMiddleware,
+    apiKeyCheckMiddleware,
+    validateData(getOrderValidation),
+    getOrder
+  );
 export default router;
