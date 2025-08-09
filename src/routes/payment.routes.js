c003a14818bdc9a80c76f6b95e496eeb668b4c1f
SoumyaAdhya007 <soumyaadhya007@gmail.com>
2025-08-09 21:06:01 +0530

    feat(routes): add  payment routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,5 +1,28 @@
 import { Router } from "express";
+import authAccessMiddleware from "../middlewares/auth.middlewares.js";
+import apiKeyCheckMiddleware from "../middlewares/apiKey.middleware.js";
+import {
+  createPayment,
+  verifyPayment,
+} from "../controllers/payment.controllers.js";
+import validateData from "../middlewares/validation.middlewares.js";
+import { createPaymentValidation } from "../validations/payment.validations.js";
 
 const router = Router();
-
+router
+  .route("/create")
+  .post(
+    authAccessMiddleware,
+    apiKeyCheckMiddleware,
+    validateData(createPaymentValidation),
+    createPayment
+  );
+router
+  .route("/verify")
+  .post(
+    authAccessMiddleware,
+    apiKeyCheckMiddleware,
+    validateData(verifyPayment),
+    verifyPayment
+  );
 export default router;
